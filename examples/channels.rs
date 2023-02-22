632360e6fc98b8f4d6771cecbae77b3d44c7c791
Connor George <connorgeorgeem@aol.com>
2023-02-22 08:26:38 +0000

    more data on channels example

--- examples/channels.rs
+++ examples/channels.rs
@@ -9,15 +9,15 @@ fn main() {
     let this_thread_send = send.clone();
 
     let _thread = thread::spawn(move || {
-        for i in 0..10 {
+        for i in 0..100 {
             send.send(i*10);
         }
     });
 
-    this_thread_send.send(100);
+    this_thread_send.send(1000);
 
-    // should output 0 -> 100 in multiples of 10. may not be in order. 0 -> 90 is sent from spawned thread. 100 is sent from this thread.
-    for _ in 0..11 {
+    // should output 0 -> 1000 in multiples of 10. may not be in order. 0 -> 90 is sent from spawned thread. 100 is sent from this thread.
+    for _ in 0..101 {
         println!("{}", recv.recv());
     }
 }
